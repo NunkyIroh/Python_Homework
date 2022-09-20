4c6515e303db5cdb816d33a86083fd9fe4d37013
Uncle_Iroh <vadegtyarev@gmail.com>
2022-09-20 17:21:41 +0300

    Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.

--- HW_Seminar_4_Task_3.py
+++ HW_Seminar_4_Task_3.py
@@ -23,3 +23,17 @@ for i in range(len(userList)):
 
 print(f'Cписок неповторяющихся элементов {uniqueList} вашего списка {userList}!')
 
+# b = [1, 1, 2, 3, 3, 4, 5]
+#  a = []
+#  for i in b:
+#     if b.count(i) == 1:
+#          a.append(i)
+
+#  print(a)
+
+
+# inp = list(map(int, input('Insert numbrers: ').split()))
+# print(set(inp))
+
+# a= [1,2,2,2,2,3,1,4]
+# print(set(a))
